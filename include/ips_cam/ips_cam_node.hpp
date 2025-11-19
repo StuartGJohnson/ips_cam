0a50a1e6142f3508745357b14edc523e212bd34b
Stuart Johnson <stuart.g.johnson@gmail.com>
2025-11-19 13:34:59 -0800

    Fix z position when translating to ros2. This is a little bit questionable, another route here is to define an aruco tag link in the robot xacro, and have this node output odom->aruco_link to the transforms and /odom.

--- include/ips_cam/ips_cam_node.hpp
+++ include/ips_cam/ips_cam_node.hpp
@@ -62,7 +62,8 @@ void from_tag_pose(TagPose & tagPose, geometry_msgs::msg::Pose & ros_pose)
   // to meters!
   ros_pose.position.x = tagPose.x * 1e-3;
   ros_pose.position.y = tagPose.y * 1e-3;
-  ros_pose.position.z = tagPose.z * 1e-3;
+  ros_pose.position.z = 0.0;
+  //ros_pose.position.z = tagPose.z * 1e-3;
   // quaternion from angle. Note this is planar motion, with rotation about zhat.
   tf2::Quaternion q;
   tf2::Vector3 z(0.0, 0.0, 1.0);
